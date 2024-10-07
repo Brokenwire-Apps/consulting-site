9b38f757648b9412541129a07f88efd4325748cc
Aaron Wallace <arttheft.situation@gmail.com>
2024-10-07 13:40:20 -0400

    Finish Navigation styles

--- src/components/nav.component.tsx
+++ src/components/nav.component.tsx
@@ -1,10 +1,18 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@awesome.me/kit-67f463dc1d/icons/sharp-duotone/solid';
+import { faBars, faClose } from '@awesome.me/kit-67f463dc1d/icons/sharp-duotone/solid';
 
 export const Nav = () => {
+    const OpenNav = () => {
+        const menu = document.querySelector('.menu');
+        const close = document.querySelector('.menu-close');
+        close?.classList.toggle('active');
+        menu?.classList.toggle('active');
+    };
+    
     return (
         <nav>
-            <FontAwesomeIcon icon={faBars} />
+            <FontAwesomeIcon className='menu-trigger' onClick={OpenNav} icon={faBars} />
+            <FontAwesomeIcon className='menu-close' onClick={OpenNav} icon={faClose} />
             <ul className="menu">
               <li><a href="#">Home</a></li>
               <li><a href="#">About</a></li>
